5c66de1f5853cd8672e8f1f1efa69afb6c20f3ed
Martín Alexis Samán Arata <martin.saman@vallegrande.edu.pe>
2020-06-22 20:36:58 -0500

    :bug: será?

--- 2/email-service/app.py
+++ 2/email-service/app.py
@@ -1,15 +1,6 @@
 from kafka_service.kafka_consumer import ConsumerService
 
-
-def listen_kafka():
-    try:
-        listener_kafka = ConsumerService()
-        listener_kafka.escuchar()
-    except Exception:
-        print('reintentando kafka')
-        listen_kafka()
-
-
 if __name__ == '__main__':
     print(":DDD")
-    listen_kafka()
+    listener_kafka = ConsumerService()
+    listener_kafka.escuchar()
