7adbb1227c6188f893989663a44f1312edc5cac7
Martín Alexis Samán Arata <martin.saman@vallegrande.edu.pe>
2020-06-22 19:40:21 -0500

    :bug: funka px :c

--- 2/email-service/app.py
+++ 2/email-service/app.py
@@ -1,27 +1,15 @@
 from kafka_service.kafka_consumer import ConsumerService
-from flask import Flask
-import threading
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def index():
-    return "Email service corriendo :D"
 
 
 def listen_kafka():
     try:
         listener_kafka = ConsumerService()
         listener_kafka.escuchar()
-    except Exception as e:
+    except Exception:
         print('reintentando kafka')
         listen_kafka()
 
 
 if __name__ == '__main__':
-    listener_kafka_hilo = threading.Thread(target=listen_kafka)
-    listener_kafka_hilo.daemon = True
-    listener_kafka_hilo.start()
     print(":DDD")
-    app.run(host='0.0.0.0', port=3009, threaded=True, debug=False)
+    listen_kafka()
