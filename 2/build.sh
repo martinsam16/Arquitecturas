0083a9b8b441e2860a0eaa7ee142eeac40de5608
Martín Alexis Samán Arata <martin.saman@vallegrande.edu.pe>
2020-06-22 22:33:36 -0500

    :bug: ya?:c

---
+++ 2/build.sh
@@ -0,0 +1,3 @@
+mvn -f ./api-gateway/pom.xml clean package
+mvn -f ./course-service/pom.xml clean package
+mvn -f ./user-service/pom.xml clean package
---
+++ 2/run.sh
@@ -0,0 +1,7 @@
+sh build.sh
+
+docker-compose build
+docker-compose up -d
+
+docker build -t email-service ./email-service
+docker run -t -d -name=email-service email-service
